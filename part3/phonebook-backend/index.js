b93c42cb7b0025f9a84f8c2270001779c62eb6e7
James Louie Grande <jlgrande@hawaii.edu>
2023-06-06 22:33:13 -1000

    Clean up

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
-
-const app = express();
 const cors = require('cors');
-require('dotenv').config();
 const morgan = require('morgan');
+require('dotenv').config();
 
 const Person = require('./models/person');
 
-const morganMiddleware = morgan(
-  ':method :url :status :res[content-length] - :response-time ms :data'
-);
-
-morgan.token('data', (req) => JSON.stringify(req.body));
-
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
+  console.error(error.message);
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
@@ -25,13 +17,22 @@ const errorHandler = (error, request, response, next) => {
   return next(error); // Check (added return)
 };
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
+
+morgan.token('data', (req) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
+
+app.get('/info', (request, response) => {
+  const date = new Date().toString();
+  Person.countDocuments({}).then((count) => {
+    response.send(`<p>Phonebook has info for ${count} people<p>${date}</p>`);
+  });
+});
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
@@ -39,11 +40,24 @@ app.get('/api/persons', (request, response) => {
   });
 });
 
-app.get('/info', (request, response) => {
-  const date = new Date().toString();
-  Person.countDocuments({}).then((count) => {
-    response.send(`<p>Phonebook has info for ${count} people<p>${date}</p>`);
-  });
+// eslint-disable-next-line consistent-return
+app.post('/api/persons', (request, response, next) => {
+  const { name, phoneNumber } = request.body;
+
+  if (!name) {
+    return response.status(400).json({ error: 'name missing' });
+  }
+
+  const person = new Person({ name, phoneNumber });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -66,36 +80,11 @@ app.delete('/api/persons/:id', (request, response, next) => {
     });
 });
 
-app.post('/api/persons', morganMiddleware, (request, response, next) => {
-  const { body } = request;
-
-  Person.find({ name: body.name }).catch(() => {
-    return response.status(409).json({ error: 'name must be unique' });
-  });
-  const person = new Person({
-    name: body.name,
-    phoneNumber: body.phoneNumber
-  });
-
-  person
-    .save()
-    .then((savedPerson) => {
-      response.json(savedPerson);
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
-
 app.put('/api/persons/:id', (request, response, next) => {
-  const { body } = request;
+  const { name, phoneNumber } = request.body;
 
-  const person = { name: body.name, phoneNumber: body.phoneNumber };
-  Person.findByIdAndUpdate(request.params.id, person, {
-    new: true,
-    runValidators: true,
-    context: 'query'
-  })
+  const person = { name, phoneNumber };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -104,7 +93,6 @@ app.put('/api/persons/:id', (request, response, next) => {
     });
 });
 
-app.use(unknownEndpoint);
 app.use(errorHandler);
 
 const { PORT } = process.env;
