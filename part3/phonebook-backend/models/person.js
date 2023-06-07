6ce3e78674ea5154376e795736429151b2227378
James Louie Grande <jlgrande@hawaii.edu>
2023-06-06 22:39:24 -1000

    Refactor

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -2,21 +2,22 @@
 /* eslint-disable no-param-reassign */
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
+
 const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url);
-
-mongoose.set('strictQuery', false);
+console.log('connecting to MongoDB in', url);
 mongoose
   .connect(url)
   .then(() => console.log('connected to MongoDB'))
   .catch((error) => console.log('error connecting to MongoDB', error.message));
 
 const personSchema = mongoose.Schema({
-  name: { type: String, minLength: 3 },
+  name: { type: String, minLength: 3, required: true },
   phoneNumber: {
     type: String,
     minLength: 8,
+    required: true,
     validate: {
       validator: (v) => {
         return /^\d{2,3}-\d+$/.test(v);
