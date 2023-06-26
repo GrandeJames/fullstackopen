a8e4ade1dc2b8e944dca14e0d67d68a8e441e3bf
James Louie Grande <jlgrande@hawaii.edu>
2023-06-25 22:04:34 -1000

    Refactor tests

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -9,70 +9,68 @@ const helper = require('./test_helper');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
-  // eslint-disable-next-line no-restricted-syntax
-  for (const blog of helper.initialBlogs) {
-    const blogObject = new Blog(blog);
-    // eslint-disable-next-line no-await-in-loop
-    await blogObject.save();
-  }
+  await Blog.insertMany(helper.initialBlogs);
 });
 
-test('blogs are returned as JSON and is the correct amount', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect('Content-Type', /application\/json/)
-    .expect(200);
+describe('GET /api/blogs', () => {
+  test('blogs are returned as JSON and is the correct amount', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect('Content-Type', /application\/json/)
+      .expect(200);
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('blogs have an id property', async () => {
+    const blogs = await helper.blogsInDB();
+    // eslint-disable-next-line no-restricted-syntax
+    for (const blog of blogs) {
+      expect(blog.id).toBeDefined();
+    }
+  });
 });
 
-test('blogs have an id property', async () => {
-  const blogs = await helper.blogsInDB();
-  // eslint-disable-next-line no-restricted-syntax
-  for (const blog of blogs) {
-    expect(blog.id).toBeDefined();
-  }
+describe('POST /api/blogs', () => {
+  test('should insert a blog to the database', async () => {
+    const blogToAdd = { title: 'blogToAdd', author: 'author', url: 'url', likes: 3 };
+    await api
+      .post('/api/blogs')
+      .send(blogToAdd)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsInDB = await helper.blogsInDB();
+    expect(blogsInDB).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsInDB.map((blog) => blog.title);
+    expect(titles).toContain(blogToAdd.title);
+  });
+
+  test('should have likes property in blog otherwise default to 0', async () => {
+    const blogToAdd = { title: 'blogToAdd', author: 'author', url: 'url' };
+
+    const response = await api
+      .post('/api/blogs')
+      .send(blogToAdd)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    if (response.body.likes) {
+      expect(response.body.likes).toBeDefined();
+    } else {
+      expect(response.body.likes).toBe(0);
+    }
+  });
+
+  test('should have title and url property in blog', async () => {
+    const blogToAdd = { author: 'author', likes: 10 };
+
+    await api.post('/api/blogs').send(blogToAdd).expect(400);
+  });
 });
 
-test('should insert a blog to the database', async () => {
-  const blogToAdd = { title: 'blogToAdd', author: 'author', url: 'url', likes: 3 };
-  await api
-    .post('/api/blogs')
-    .send(blogToAdd)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsInDB = await helper.blogsInDB();
-  expect(blogsInDB).toHaveLength(helper.initialBlogs.length + 1);
-
-  const titles = blogsInDB.map((blog) => blog.title);
-  expect(titles).toContain(blogToAdd.title);
-});
-
-test('should have likes property in blog otherwise default to 0', async () => {
-  const blogToAdd = { title: 'blogToAdd', author: 'author', url: 'url' };
-
-  const response = await api
-    .post('/api/blogs')
-    .send(blogToAdd)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  if (response.body.likes) {
-    expect(response.body.likes).toBeDefined();
-  } else {
-    expect(response.body.likes).toBe(0);
-  }
-});
-
-test('should have title and url property in blog', async () => {
-  const blogToAdd = { author: 'author', likes: 10 };
-
-  const response = await api.post('/api/blogs').send(blogToAdd).expect(400);
-});
-
-describe('viewing a specific blog', () => {
+describe('DELETE /api/blogs/:id', () => {
   test('succeeds with valid id', async () => {
     const blogs = await helper.blogsInDB();
     const blogToDelete = blogs[0];
@@ -88,16 +86,18 @@ describe('viewing a specific blog', () => {
   });
 });
 
-test('should update blog with valid id', async () => {
-  const blogs = await helper.blogsInDB();
-  const blogToUpdate = blogs[0];
-  const updatedBlog = { title: 'blogToUpdate', author: 'author', url: 'url', likes: -1 };
+describe('PUT /api/blogs/:id', () => {
+  test('should update blog with valid id', async () => {
+    const blogs = await helper.blogsInDB();
+    const blogToUpdate = blogs[0];
+    const updatedBlog = { title: 'blogToUpdate', author: 'author', url: 'url', likes: -1 };
 
-  await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(200);
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(200);
 
-  const updatedBlogs = await helper.blogsInDB();
-  const likes = updatedBlogs.map((blog) => blog.likes);
-  expect(likes).toContain(updatedBlog.likes);
+    const updatedBlogs = await helper.blogsInDB();
+    const likes = updatedBlogs.map((blog) => blog.likes);
+    expect(likes).toContain(updatedBlog.likes);
+  });
 });
 
 afterAll(async () => {
