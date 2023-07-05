96cf6e298f39fb3c7bce66b1b4a94db8d94972c3
James Louie Grande <jlgrande@hawaii.edu>
2023-07-04 14:38:35 -1000

    Complete task 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,25 +1,34 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body);
+  const { body } = request;
 
-  if (!blog.title && !blog.url) {
-    response.status(400).end();
-    return;
+  if (!body.title && !body.url) {
+    return response.status(400).end();
   }
 
-  if (!blog.likes) {
-    blog.likes = 0;
-  }
+  const user = await User.findOne({});
+
+  const blog = new Blog({
+    url: body.url,
+    title: body.title,
+    likes: body.likes ? body.likes : 0,
+    user: user._id
+  });
 
   const result = await blog.save();
+
+  user.blogs = user.blogs.concat(result._id);
+  await user.save();
+
   response.status(201).json(result);
 });
 
