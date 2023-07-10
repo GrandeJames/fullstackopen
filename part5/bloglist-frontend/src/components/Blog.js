a27487abf55525a70b2ead5c5d5be1d6d14cd1a2
James Louie Grande <jlgrande@hawaii.edu>
2023-07-10 09:59:38 -1000

    Complete task 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div key={blog.id}>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const blogStyle = {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#dad7cd",
+    margin: 5,
+  };
+
+  return (
+    <div key={blog.id} style={blogStyle}>
+      {blog.title} {blog.author}
+      <button type="button" onClick={() => setExpanded(!expanded)}>
+        {expanded ? "hide" : "view"}
+      </button>
+      <div style={{ display: expanded ? "" : "none" }}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes ? blog.likes : 0}
+          <button type="button">like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
