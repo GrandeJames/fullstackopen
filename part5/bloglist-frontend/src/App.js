41bc0dfdca5c6f99c825eaa27e9ff94eda9166cd
James Louie Grande <jlgrande@hawaii.edu>
2023-07-10 09:59:57 -1000

    Clean code

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,30 +1,24 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import blogService from "./services/blogs";
 import Blogs from "./components/Blogs";
 import Login from "./components/Login";
-import CreateBlogForm from "./components/CreateBlogForm";
-import Togglable from "./components/Togglable";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
   const [notification, setNotification] = useState(null);
-
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
-  }, []);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedUser");
     if (loggedUserJSON) {
       const loggedUser = JSON.parse(loggedUserJSON);
+
       setUser(loggedUser);
       blogService.setToken(loggedUser.token);
     }
   }, []);
 
-  const handleAddBlog = (newBlog) => {
-    setBlogs(blogs.concat(newBlog));
+  const handleUser = (newUser) => {
+    setUser(newUser);
   };
 
   const handleNotification = (newNotification) => {
@@ -35,29 +29,16 @@ const App = () => {
     }, 5000);
   };
 
-  const handleUser = (newUser) => {
-    setUser(newUser);
-  };
-
-  const addBlogRef = useRef();
-
   return (
     <>
       {user ? (
         <>
           <Blogs
-            blogs={blogs}
             user={user}
             notification={notification}
             handleUser={handleUser}
+            handleNotification={handleNotification}
           ></Blogs>
-          <Togglable buttonLabel="add blog" ref={addBlogRef}>
-            <CreateBlogForm
-              toggleVisibility={() => addBlogRef.current.toggleVisibility()}
-              addBlog={handleAddBlog}
-              handleNotification={handleNotification}
-            ></CreateBlogForm>
-          </Togglable>
         </>
       ) : (
         <Login
--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -1,26 +1,51 @@
 import Blog from "./Blog";
 import Notification from "./Notification";
+import Togglable from "./Togglable";
+import CreateBlogForm from "./CreateBlogForm";
+import { useRef, useEffect, useState } from "react";
+import blogService from "../services/blogs";
 
-const Blogs = ({ blogs, handleUser, user, notification }) => {
+const Blogs = ({ handleUser, user, notification, handleNotification }) => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
   const handleLogout = () => {
     window.localStorage.removeItem("loggedUser");
     handleUser(null);
   };
+
+  const handleAddBlog = (newBlog) => {
+    setBlogs(blogs.concat(newBlog));
+  };
+
+  const addBlogRef = useRef();
   return (
     <div>
       <h2>blogs</h2>
+      {notification && (
+        <Notification notification={notification}></Notification>
+      )}
       <div>
-        {notification && (
-          <Notification notification={notification}></Notification>
-        )}
         {user.name} logged in{" "}
         <button type="button" onClick={handleLogout}>
           logout
         </button>
       </div>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      <Togglable buttonLabel="add blog" ref={addBlogRef}>
+        <CreateBlogForm
+          toggleVisibility={() => addBlogRef.current.toggleVisibility()}
+          addBlog={handleAddBlog}
+          handleNotification={handleNotification}
+        ></CreateBlogForm>
+      </Togglable>
+      <div>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
     </div>
   );
 };
