927eed983d66be91837abceadd2b402a906c13b6
James Louie Grande <jlgrande@hawaii.edu>
2023-07-16 11:05:06 -1000

    Complete task 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,30 +1,45 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Blog from './Blog';
+import userEvent from '@testing-library/user-event';
 
-test('blog is not expanded by default', () => {
-    const blog = {
-        id: 'id',
-        likes: 0,
-        title: 'title',
-        url: 'url',
-        author: 'author',
-        user: {
+describe('<Blog />', () => {
+    let container;
+
+    beforeEach(() => {
+        const blog = {
+            id: 'id',
+            likes: 0,
+            title: 'title',
+            url: 'url',
+            author: 'author',
+            user: {
+                username: 'username'
+            }
+        };
+        const user = {
             username: 'username'
-        }
-    };
+        };
+        const updateBlog = jest.fn();
+        const removeBlog = jest.fn();
 
-    const user = {
-        username: 'username'
-    };
+        container = render(
+            <Blog blog={blog} updateBlog={updateBlog} removeBlog={removeBlog} user={user} />
+        ).container;
+    });
 
-    const updateBlog = jest.fn();
-    const removeBlog = jest.fn();
+    test('not expanded by default', () => {
+        const element = container.querySelector('.expandedContent');
+        expect(element).toHaveStyle('display: none');
+    });
 
-    const { container } = render(
-        <Blog blog={blog} updateBlog={updateBlog} removeBlog={removeBlog} user={user} />
-    );
+    test('expanded on view click', async () => {
+        const user = userEvent.setup();
 
-    const element = container.querySelector('.expandedContent');
-    expect(element).toHaveStyle('display: none');
+        const element = container.querySelector('.expandedContent');
+        const viewButton = screen.getByText('view');
+
+        await user.click(viewButton);
+        expect(element).not.toHaveStyle('display: none');
+    });
 });
